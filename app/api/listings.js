1e09e25e94c91e3e84546ff4e7971e771ea9a7d8
iamfortune <ikechifortune@gmail.com>
2020-09-27 11:07:38 +0100

    added an upload screen file

--- app/api/listings.js
+++ app/api/listings.js
@@ -4,6 +4,30 @@ const endpoint = '/listings';
 
 const getListings = () => client.get(endpoint);
 
-export default {
-    getListings,
+export const addListing = (listing, onUploadProgress) => {
+    const data = new FormData();
+    data.append('title', listing.title);
+    data.append('price', listing.price);
+    data.append('categoryId', listing.category.value);
+    data.append('description', listing.description);
+
+    listing.images.forEach((image, index) => 
+    data.append('images', {
+        name: 'image' + index,
+        type: 'image/jpeg',
+        uri: image
+    }));
+
+    if (listing.location) 
+    data.append('location', JSON.stringify(listing.location));
+
+    return client.post(endpoint, data, {
+        onUploadProgress: (progress) => 
+        onUploadProgress(progress.loaded / progress.total)
+    });
 }
+
+export default {
+    addListing,
+    getListings,
+};
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -11,6 +11,7 @@ import Screen from "../components/Screen";
 import CategoryPickerItem from '../components/Picker/CategoryPickerItem';
 import FormImagePicker from "../components/forms/FormImagePicker";
 import { ScrollView } from "react-native-gesture-handler";
+import listingsApi from '../api/listings';
 import useLocation from "../hooks/useLocation";
 
 const validationSchema = Yup.object().shape({
@@ -81,6 +82,17 @@ const categories = [
 function ListingEditScreen() {
   const location = useLocation();
 
+   const handleSubmit = async (listing) => {
+    const result = await listingsApi.addListing(
+      { ...listing, location },
+      progress => console.log(progress)
+      );
+    if (!result.ok) 
+      return alert('Could not save listing.');
+      alert('listing created successfully!')
+  }
+
+
   return (
     <ScrollView>
     <Screen style={styles.container}>
@@ -93,7 +105,7 @@ function ListingEditScreen() {
           category: null,
           images: [],
         }}
-        onSubmit={(values) => console.log(location)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
         <FormImagePicker name="images" />
